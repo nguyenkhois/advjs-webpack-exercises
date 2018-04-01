a59aee08af57e76dd990e40c51752a16043bfcb7
Khoi Le <minarek@outlook.com>
2018-04-01 22:10:07 +0200

    Created webpack exercises

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import $ from 'jquery'
+
+$("#dspMessage").text("Hello webpack with jQuery & webpack-dev-server?");
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const WebpackNotifierPlugin = require("webpack-notifier");
+
+module.exports = {
+    mode: 'development', // whether we're building for dev or prod
+    entry: './src/index.js', // which file to begin with
+    output: {
+        path: path.resolve(__dirname, "dist"), // what folder to put bundle in
+        filename: 'bundle.js' // what name to use for bundle
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "./src"), // the root for the server
+        watchContentBase: true, // so we reload if other stuff like CSS changes
+        port: 9000, // it'll now be at http://localhost:9000
+    },
+    plugins: [
+        new WebpackNotifierPlugin({alwaysNotify: true}),
+    ]
+};
